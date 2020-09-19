5369259b5e3e20d74e2fbae7a5fbe40d4326d3bf
cristianeasreis <cristianeasreis@hotmail.com>
2020-09-19 03:10:18 -0300

    desafio ordenação de filtros

---
+++ Ordenação e Filtros em JavaScript/desafio2.js
@@ -0,0 +1,20 @@
+// Ordenação e Filtros em JavaScript
+// 2 / 5 - Compras no Supermercado
+
+let num = 0;
+let index = 0;
+
+num = parseInt(gets());
+
+let listas = Array(num);
+let listaSet = Array(num);
+
+while (index < num ) {
+    listas[index] = gets().split(" ");
+    index++;
+};
+
+listas.map((i,d) => listaSet[d] = new Set(i));
+listaSet.map((i,d) => listas[d] = [...i]);
+listas.forEach(i => i.sort());
+listas.forEach(i => console.log(i.join(" ")));
